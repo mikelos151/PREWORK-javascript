c195f2427772d5b109c4a8805f3d45061411c6f7
mikelos151 <95861270+mikelos151@users.noreply.github.com>
2021-12-17 10:29:06 +0100

    Update zadanie34.js

--- zadanie34/zadanie34.js
+++ zadanie34/zadanie34.js
@@ -1,9 +1,11 @@
 let firstNumber  = 10 ;
 let secondNumber = 30 ;
 
-console.log(firstNumber)
-console.log(secondNumber)
+console.log(firstNumber) ;
+console.log(secondNumber) ;
 
-let result = firstNumber + secondNumber
+function sum(firstnumber, secondnumber) {let result = firstNumber + secondNumber ;
+return result;
+}
 
-console.log(result)
+console.log(sum(firstNumber + secondNumber))
