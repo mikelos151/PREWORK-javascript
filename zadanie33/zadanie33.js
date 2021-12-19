4c0e8c1938c1f69dc2619e64eeebbc892c1f8e27
mikelos151 <95861270+mikelos151@users.noreply.github.com>
2021-12-19 16:38:11 +0100

    Update zadanie33.js

--- zadanie33/zadanie33.js
+++ zadanie33/zadanie33.js
@@ -5,5 +5,4 @@ console.log(number)
 if ( number > 10) 
 { console.log('Podana liczba jest większa od 10') ; } 
 
-else (number < 10)
- {console.log('Podana liczba nie jest większa od 10'); }
+else {console.log('Podana liczba nie jest większa od 10'); }
